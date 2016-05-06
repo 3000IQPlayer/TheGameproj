8abbbd47298edba59d5ee840e9b393bd52ae09b8
Tehdemon <ghille2010@gmail.com>
2016-05-06 16:33:12 -0700

    The project

---
+++ Other.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.ArrayList;
+
+class Other
+{
+
+ public static void main(String str[]) throws IOException
+ {
+ Scanner scan = new Scanner(System.in);
+ ArrayList <String> list = new ArrayList <String>();
+
+ 
+  System.out.println("Welcome to 20 Questions to become a Millionare Game!");
+  System.out.println("Type \"PLAY\" to start your questions to have a chance to become a millionare!");
+  String a; 
+  a = scan.nextLine();
+
+ 
+
+ }
+}
